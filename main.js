c899492387f9be639746f05da80d1f5b230aea9d
Jeet <bhattjeet110@gmail.com>
2024-07-15 18:36:03 +0530

    add todoes and remove todoes in dom - 15-07-2024

--- main.js
+++ main.js
@@ -2,98 +2,77 @@ const form = document.querySelector("#itemForm");
 const titleInput = document.querySelector("#title");
 const selectInput = document.querySelector("#select");
 const itemList = document.querySelector("#itemList");
-const filters = document.querySelectorAll(".filters");
 
-// create empty item list
-let todoItems = [];
+let todos = [];
 
-//hendle action buttion event
-const handleItem = function (itemData) {
-  const items = document.querySelectorAll(".messageBox");
-  items.forEach((item) => {
-    if (
-      item.querySelector(".vl").getAttribute("data-time") ===
-      String(itemData.addedAt)
-    ) {
-      // done
-      item.querySelector("[data-done]").addEventListener("click", function (e) {
-        e.preventDefault();
-        const itemIndex = todoItems.indexOf(itemData);
-        const currentItem = todoItems[itemIndex];
-        currentItem.isDone = currentItem.isDone ? false : true;
-        todoItems.splice(itemIndex, 1, currentItem);
-        setLocalStorage(todoItems);
-      });
-        
-    }
+// common code for onLoad UI and localStorage
+function createListItem(todoName, todoSelect, itemList) {
+  const li = document.createElement("li");
+  li.setAttribute("class", "messageBox");
+  li.innerHTML = `${todoName}<br>${todoSelect}`;
+
+  // delete Todoes
+  const deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fa-solid fa-trash-can");
+  deleteIcon.addEventListener("click", function (event) {
+    // alert();
+    // remove li from DOM
+    console.log(event.target);
+    event.target.parentElement.remove();
+    // remove li from todos array
+    // remove li from localStorage
   });
-};
+  li.appendChild(deleteIcon);
+  itemList.appendChild(li);
+}
 
-// get todos list
-const getList = function (todoItems) {
-  itemList.innerHTML = "";
-  if (todoItems.length > 0) {
-    todoItems.forEach((item) => {
-      itemList.insertAdjacentHTML(
-        "beforeend",
-        `<div class="messageBox">
-                <div data-time="${item.addedAt}" class="vl" >
-                    <div style="margin-left: 1rem;">
-                        <h5>${item.name}</h5>
-                        <p>${item.select}</p>
-                    </div>
-                </div>
-                <div>
-                    <a  data-done  class="clear-button">
-                        <i class="fa-solid fa-check"></i>
-                    </a>
-                    <a href="#" data-delete class="clear-button">
-                        <i class="fas fa-trash-alt"></i>
-                    </a>
-                <div>
-            </div>`
-      );
-      handleItem(item);
-    });
-  }
-};
-
-// get localStorage from the page
-const getLocalStorage = function () {
-  const todoStorage = localStorage.getItem("todoItems");
-  if (todoStorage === "undefined" || todoStorage === "null") {
-    todoItems = [];
-  } else {
-    todoItems = JSON.parse(todoStorage);
-  }
-  console.log("items", todoItems);
-  getList(todoItems);
-};
-
-// set in localStorage
-const setLocalStorage = function (todoItems) {
-  localStorage.setItem("todoItems", JSON.stringify(todoItems));
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const itemName = titleInput.value;
-    const itemImportance = selectInput.value;
-    if (itemName.length === 0 || itemImportance.length === 0) {
-      alert("Please enter the fields");
-    } else {
-      const itemObj = {
-        name: itemName,
-        select: itemImportance,
-        isDone: false,
-        addedAt: new Date().getTime(),
-      };
-      todoItems.push(itemObj);
-      setLocalStorage(todoItems);
-      //   getList(todoItems); // Update the list after adding a new item
-    }
+// for ui dispaly
+function renderTodos(todos) {
+  // loop overly an arry, and create li element and append to the doM
+  todos.forEach(function (todo) {
+    // let li = document.createElement("li");
+    // li.setAttribute("class", "messageBox");
+    // li.innerHTML = `${todo.name}<br>${todo.select}`;
+    // itemList.appendChild(li);
+    createListItem(todo.name, todo.select, itemList); // common code of onload ui and storage createListItem
   });
-  // load Items
-  getLocalStorage();
+}
+
+//check is todos exixt in local-Storage
+const storedTodos = localStorage.getItem("todos");
+// if exist
+if (storedTodos) {
+  // pars todo JSone.parse todos
+  const parsestoredTodos = JSON.parse(storedTodos);
+  todos = parsestoredTodos; // to prevent exesting valiu ovverride
+  console.log(parsestoredTodos);
+  renderTodos(todos); // call renderTodos function on load
+}
+
+itemForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //   console.log("hii");
+  //   console.log(itemForm.title.value, itemForm.select.value);
+  //  console.log(titleInput.value, selectInput.value);
+  let todoName = titleInput.value;
+  let todoSelect = selectInput.value;
+
+  const itemObj = {
+    name: todoName,
+    select: todoSelect,
+    completed: false,
+  };
+  todos.push(itemObj);
+
+  titleInput.value = "";
+  selectInput.value = "";
+
+  localStorage.setItem("todos", JSON.stringify(todos)); // set my todos data in localStorage
+  //create li
+  //   const li = document.createElement("li");
+  // set in the innerHtml and set in the class
+  //   li.setAttribute("class", "messageBox");
+  //   li.innerHTML = `${todoName}<br>${todoSelect}`;
+  //   itemList.appendChild(li);
+  createListItem(todoName, todoSelect, itemList); // common code of onload ui and storage createListItem
 });
